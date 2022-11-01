cf0c7a3fa4af6619392ce9e37c5ca9bd32f84773
SNAVANEETHAKRISHNAN <kriz180@gmail.com>
2022-11-01 18:24:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function Person() {
+    this.firstName = "unknown";
+    this.lastName = "unknown";
+
+    this.mobile ="unknown";
+
+    this.email ="unknown";
+
+    this.city="unknown";
+
+    this.state ="unknown";
+
+    
+}
+
+var person2= new Person();
+person2.firstName="guvi";
+person2.lastName="geek";
+person2.mobile="0000000000";
+person2.email="abc@gmail.com";
+person2.city="chennai";
+person2.state="Tamil Nadu";
+
+console.log(person2);
